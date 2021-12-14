0a433626c79725f9a76cb4283f3b9dd4270d94c9
Nick Watt <nick@gtctek.co.uk>
2021-12-14 23:29:51 +0000

    Query & num returned added to search results page

--- wp-content/themes/fitdad/search.php
+++ wp-content/themes/fitdad/search.php
@@ -4,6 +4,21 @@
 		<div class="row">
 			<div class="col-12">
 				<?php
+					printf( esc_html( get_search_query() ) );
+
+					printf(
+						esc_html(
+							/* translators: %d: The number of search results. */
+							_n(
+								'We found %d result for your search.',
+								'We found %d results for your search.',
+								(int) $wp_query->found_posts,
+								'twentytwentyone'
+							)
+						),
+						(int) $wp_query->found_posts
+					);
+
 					if(have_posts()) : while(have_posts()) : the_post();
 						the_content();
 					endwhile; else : endif;
