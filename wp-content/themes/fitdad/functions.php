92f17dcdea6431715137b1088e2333dd8c2bd6aa
Nick Watt <nick@gtctek.co.uk>
2022-09-17 20:20:11 +0100

    Updated app.js ver to 1.0.2

--- wp-content/themes/fitdad/functions.php
+++ wp-content/themes/fitdad/functions.php
@@ -9,7 +9,7 @@ add_action('wp_enqueue_scripts', 'load_loadstylesheets');
 
 function load_javascript()
 {
-	wp_register_script('custom', get_template_directory_uri() . '/js/app.js', 'jquery', 1, true);
+	wp_register_script('custom', get_template_directory_uri() . '/js/app.js', 'jquery', '1.0.2', true);
 	wp_enqueue_script('custom');
 }
 add_action('wp_enqueue_scripts', 'load_javascript');
