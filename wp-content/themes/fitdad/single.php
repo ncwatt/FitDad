cc2287f9d435e6811e9e69ce3465f41963de2fd1
Nick Watt <nwatt@hotmail.com>
2022-11-18 23:28:24 +0000

    Comments template section changed to bootstrap row

--- wp-content/themes/fitdad/single.php
+++ wp-content/themes/fitdad/single.php
@@ -3,28 +3,37 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-lg-9 post-single">
-				<?php
-					// Check if there is a featured image
-					if ( has_post_thumbnail() ) {
-						// Display the thumbnail
-						echo "<img src=\"", get_the_post_thumbnail_url(), "\" class=\"featured-image\" alt=\"", get_the_title(), "\" />";
-					}
-					else {
-						// No thumbnail image so lets display the default one from the template
-						echo "<img src=\"", bloginfo('template_directory'), "/img/default-post-image.jpg\" class=\"featured-image\" alt=\"", get_the_title(), "\" />";
-					}
+				<div class="row">
+					<div class="col-12">
+						<?php
+							// Check if there is a featured image
+							if ( has_post_thumbnail() ) {
+								// Display the thumbnail
+								echo "<img src=\"", get_the_post_thumbnail_url(), "\" class=\"featured-image\" alt=\"", get_the_title(), "\" />";
+							}
+							else {
+								// No thumbnail image so lets display the default one from the template
+								echo "<img src=\"", bloginfo('template_directory'), "/img/default-post-image.jpg\" class=\"featured-image\" alt=\"", get_the_title(), "\" />";
+							}
 
-					// Display the title
-					echo "<h1>", get_the_title(), "</h1>";
-					echo "<p class=\"post-datetime\">Posted: ", get_post_time('d M Y H:i'), "</p>";
-					echo the_content();
-					echo the_tags();
-
-					// If comments are open or there is at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				?>
+							// Display the title
+							echo "<h1>", get_the_title(), "</h1>";
+							echo "<p class=\"post-datetime\">Posted: ", get_post_time('d M Y H:i'), "</p>";
+							echo the_content();
+							echo the_tags();
+						?>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12 col-lg-9 comments">
+						<?php
+							// If comments are open or there is at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) {
+								comments_template();
+							}
+						?>
+					</div>
+				</div>
 			</div>
 			<div class="col-12 col-lg-3">
 				<div class="row">
