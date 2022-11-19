8b9a1d07f205720fa477d1f71226251f896b3102
Nick Watt <nwatt@hotmail.com>
2022-11-19 00:07:59 +0000

    First comments list

--- wp-content/themes/fitdad/comments.php
+++ wp-content/themes/fitdad/comments.php
@@ -1,18 +1,69 @@
-<?php if(!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) : ?>
-<?php endif; ?>
-	
-<?php if(!empty($post->post_password)) : ?>
-	<?php if($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) : ?>
-	<?php endif; ?>
-<?php endif; ?>
+<?php
+	/*
+ 	* If the current post is protected by a password and the visitor has not yet
+ 	* entered the password we will return early without loading the comments.
+ 	*/
+	if (post_password_required())
+ 		return;
 
-<?php if($comments) : ?>
-	<?php foreach($comments as $comment) : ?>
-		<?php if ($comment->comment_approved == '0') : ?>
+	// Get the sort order for comments from site settings
+	$comment_order = get_option('comment_order');
+
+	$args = array(
+		//'status' => 'approve',
+		'post_id' => get_the_ID(),	// Post ID of the current post
+		'order' => $comment_order	// Order to display comments
+	);
+
+	// Build the comments query
+	$comments_query = new WP_Comment_Query();
+	$comments = $comments_query->query($args);
+?>
+
+
+<h2>Comments</h2>
+<?php if ($comments) : ?>
+	<?php foreach ($comments as $comment) : ?>
+		<?php if ($comment->comment_approved == '1') : ?>
+			<div class="comment-header">
+				<a name="comment-<?php echo $comment->comment_ID; ?>"></a>
+				<?php echo get_avatar($comment->comment_author_email, $size = '60'); ?>
+				<p>
+					<?php 
+						if ($comment->comment_author_url != '') :
+							echo "<a href=\"" . $comment->comment_author_url . "\" target=\"_blank\">" . $comment->comment_author . "</a>";
+						else :
+							echo $comment->comment_author;
+						endif;
+					?>
+					<br />
+					<?php echo date_format(new DateTime($comment->comment_date), 'd M Y H:i'); ?>
+				</p>
+			</div>
+			<div class="comment-content">
+				<?php echo str_replace(chr(13), "<br />", $comment->comment_content); ?>
+			</div>
+		<?php else : ?>
+			<div class="comment-header">
+				<a name="comment-<?php echo $comment->comment_ID; ?>"></a>
+				<?php echo get_avatar($comment->comment_author_email, $size = '60'); ?>
+				<p>
+					<?php echo $comment->comment_author; ?>
+					<br />
+					<?php echo date_format(new DateTime($comment->comment_date), 'd M Y H:i'); ?>
+				</p>
+			</div>
+			<div class="comment-content">
+				</i>This comment is awaiting moderation</i>
+			</div>
 		<?php endif; ?>
+		<hr />
 	<?php endforeach; ?>
 <?php else : ?>
+	<p>There are currently no comments. Why not be the first by using the form below.</p>
 <?php endif; ?>
+
+
 
 <h2>Leave a comment</h2>
 <?php if(comments_open()) : ?>
